f9389b477387c9a4b218e89674457a2afbcb3a6a
TuretskayaIrina <turetskaya30.07.1994@gmail.com>
2020-10-18 19:34:05 +0300

    feat: crash-test

--- app.js
+++ app.js
@@ -30,6 +30,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger); // логгер запросов
 
+// crash-test
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),

aab426863e5d422509338d059ba8c35188b7feba
TuretskayaIrina <turetskaya30.07.1994@gmail.com>
2020-10-19 22:35:52 +0300

    hotfix: auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken');
 const AuthError = require('../errors/auth-error');
+
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
-  if (!authorization && !authorization.startsWith('Bearer ')) {
+  if (!authorization || !authorization.startsWith('Bearer ')) {
     next(new AuthError('Необходима авторизация'));
   }
 
